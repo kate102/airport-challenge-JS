7866d294fde9d3552d37e30486d8fea1f6c5b4f4
Kate102 <student@Admins-MacBook-Pro-4.local>
2019-03-05 15:57:15 +0000

    added plane

--- src/airport.js
+++ src/airport.js
@@ -3,24 +3,12 @@
 function Airport() {
   this.capacity = 50
 }
-// 
-//   attr_writer :full, :capacity
-//
-//   def initialize(full = false)
-//     @capacity = 50
-//     @full = full
-//   end
 
 Airport.prototype.setCapacity = function(capacity) {
   this.capacity = capacity
   return "Airport capacity is now " + this.capacity
 }
 
-//   def capacity(capacity)
-//     @capacity = capacity
-//     p "Airport capacity is now #{@capacity}"
-//   end
-//
 //   def full?
 //     @full
 //   end
