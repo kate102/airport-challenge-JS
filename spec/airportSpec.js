c09c61a65bc6acfa32aee793b690bcd60509a56d
Kate102 <student@Admins-MacBook-Pro-4.local>
2019-03-05 15:37:19 +0000

    passed first test to create an airport object

---
+++ spec/airportSpec.js
@@ -0,0 +1,13 @@
+describe ('Airport', function(){
+
+// Arrange
+  it ('must create a airport object, default is not null',function() {
+    airport = new Airport();
+});
+//   it "must be able to change the capacity " do
+//     airport = Airport.new
+//     expect(airport.capacity(1000)).to eq "Airport capacity is now 1000"
+//   end
+//
+// end
+});
